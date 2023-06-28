a80d5c25b45434924d38348fc67100101fc9ecd3
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-28 10:02:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+const removeButton = document.getElementById("removeButton");
+
+// Add event listener to the remove button
+removeButton.addEventListener("click", () => {
+  let selectedIndex = colorSelect.selectedIndex;
+  if (selectedIndex >= 0) {
+    colorSelect.remove(selectedIndex);
+  }
+});
